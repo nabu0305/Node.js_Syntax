89b219e27ea13b50a6aac262c4598867a0baf9d6
nabu0305 <nabu010305@gmail.com>
2022-03-21 10:37:55 +0000

    readfiledir

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Node.js_Syntax
+
+## How do you get a list of the names of all files present in a directory in Node.js?
+
+```JavaScript
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
+});
+```
+
+
+```JavaScript
+var fs = require('fs');
+fs.readFile('sample.txt','utf-8',(err, data)=>{
+    console.log(data);  
+});
+```
--- main.js
+++ main.js
@@ -6,38 +6,76 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
 	var title = queryData.id;
 	var queryData = url.parse(_url, true).query;
-    if(_url == '/'){
-      title = Welcome;
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-	fs.readFile(`data/${queryData.id}`,'utf-8',(err,description)=>{
-		var template = `
-		<!doctype html>
-		<html>
-		<head>
-		  <title>WEB1 - ${title}</title>
-		  <meta charset="utf-8">
-		</head>
-		<body>
-		  <h1><a href="index.html">WEB</a></h1>
-		  <ol>
-			<li><a href="?id=HTML">HTML</a></li>
-			<li><a href="?id=CSS">CSS</a></li>
-			<li><a href="?id=JavaScript">JavaScript</a></li>
-		  </ol>
-		  <h2>${title}</h2>
-		  <p>${description}</p>
-		</body>
-		</html>
-		`
-	})
+    var pathname = url.parse(_url,true).pathname;
+
+	if(pathname === '/'){
+		if(queryData.id === undefined){
+			fs.readdir('./data', (err, filelist) => {
+				var list = '<ul>';
+				var title = 'Welcome';
+				var description = 'Hello Node js';
+				var i = 0;
+				while(i < filelist.length){
+					list = list +`<li><a 
+					href="?id=${filelist}">${filelist}</a></li>
+					`
+					i = i+1;
+				}
+				list = list + '</ul>';
+				var template = `
+				<!doctype html>
+				<html>
+				<head>
+				  <title>WEB1 - ${title}</title>
+				  <meta charset="utf-8">
+				</head>
+				<body>
+				  <h1><a href="index.html">WEB</a></h1>
+				  <ol>
+					<li><a href="?id=HTML">HTML</a></li>
+					<li><a href="?id=CSS">CSS</a></li>
+					<li><a href="?id=JavaScript">JavaScript</a></li>
+				  </ol>
+				  <h2>${title}</h2>
+				  <p>${description}</p>
+				</body>
+				</html>
+				`;
+				response.writeHead(200);
+				response.end(template);
+			  });
+			
+		}
+		else{
+			fs.readFile(`data/${queryData.id}`,'utf-8',(err,description)=>{
+				var template = `
+				<!doctype html>
+				<html>
+				<head>
+				  <title>WEB1 - ${title}</title>
+				  <meta charset="utf-8">
+				</head>
+				<body>
+				  <h1><a href="index.html">WEB</a></h1>
+				  <ol>
+					<li><a href="?id=HTML">HTML</a></li>
+					<li><a href="?id=CSS">CSS</a></li>
+					<li><a href="?id=JavaScript">JavaScript</a></li>
+				  </ol>
+				  <h2>${title}</h2>
+				  <p>${description}</p>
+				</body>
+				</html>
+				`;
+				response.writeHead(200);
+				response.end(template);
+			});
+		}
+	}
+	else{
+		response.writeHead(404);
+		response.end('Not found');
+	}
 	
-    response.end(template);
- 
 });
 app.listen(3000);
